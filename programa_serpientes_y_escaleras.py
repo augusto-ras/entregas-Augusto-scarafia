2b00236d165e2790ee143999ddca2fcd30726b65
augusto-ras <augustorscarafia@gmail.com>
2025-07-03 17:16:25 -0300

    Update programa_serpientes_y_escaleras.py

--- programa_serpientes_y_escaleras.py
+++ programa_serpientes_y_escaleras.py
@@ -2,33 +2,39 @@
 from preguntas import preguntas
 from funsiones_s import *
 import random
-posision_deljugador=15
+posicion_del_jugador=15
 tablero=[0,1,0,0,0,3,0,0,0,0,0,1,0,0,2,1,1,0,0,0,1,0,0,2,0,0,0,1,0,0,0]
 lista_ya_preguntadas=[]
 
 nombre_usuario=pedir_nombre_usuario()
 estado_de_juego=elige_estado_de_juego()
-while estado_de_juego=="S" or estado_de_juego=="s":
- n_preguta=random.randint(0, 14)
- while numero_aleatoria_que_no_se_repite(n_preguta,lista_ya_preguntadas)== False:
+hay_preguntas = True 
+while (estado_de_juego=="S" or estado_de_juego=="s")and hay_preguntas==True :
+ if len(lista_ya_preguntadas) == len(preguntas):
+     print("¡Se han terminado las preguntas!")
+     estado_de_juego = "N"
+     hay_preguntas= False
+ else:
   n_preguta=random.randint(0, 14)
-  numero_aleatoria_que_no_se_repite(n_preguta,lista_ya_preguntadas)
- pregunta(preguntas,n_preguta)
- resultado_pregunta=comprueba_la_respuesta(preguntas,n_preguta,input_usario())
- print("tu respuesta es",resultado_pregunta)
- lista_ya_preguntadas.append(n_preguta)
- posision_deljugador=movimiento(posision_deljugador,tablero,resultado_pregunta)
- print("tu posision actual es: ",posision_deljugador,"\n")
- estado_de_juego=ganaste_o_perdiste(posision_deljugador)
- if estado_de_juego=="S" or estado_de_juego=="s":
+  while numero_aleatoria_que_no_se_repite(n_preguta,lista_ya_preguntadas)== False:
+   n_preguta=random.randint(0, 14)
+  pregunta(preguntas,n_preguta)
+  resultado_pregunta=comprueba_la_respuesta(preguntas,n_preguta,input_usuario())
+  print("tu respuesta es",resultado_pregunta)
+  lista_ya_preguntadas.append(n_preguta)
+  posicion_del_jugador=movimiento(posicion_del_jugador,tablero,resultado_pregunta)
+  print("tu posision actual es: ",posicion_del_jugador,"\n")
+  estado_de_juego=ganaste_o_perdiste(posicion_del_jugador)
+  if estado_de_juego=="S" or estado_de_juego=="s":
     estado_de_juego=elige_estado_de_juego()
  
- 
-  
-if estado_de_juego=="N" or estado_de_juego=="n":
- print("tu posision finla es: ",posision_deljugador)
+if estado_de_juego=="N" or estado_de_juego=="n" :
+ print("tu posision finla es: ",posicion_del_jugador)
  print("Fin del Juego")
- archivos_csv(nombre_usuario,posision_deljugador)
+ archivos_csv(nombre_usuario,posicion_del_jugador)
+
+
+
 
 
 
